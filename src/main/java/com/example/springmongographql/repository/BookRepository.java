cfae93f81adedd72d0391e07bf6e4f35cd7bd135
vojtechpavlu <79374331+vojtechpavlu@users.noreply.github.com>
2024-02-21 18:03:00 +0100

    Searching by Tag (#3)

--- src/main/java/com/example/springmongographql/repository/BookRepository.java
+++ src/main/java/com/example/springmongographql/repository/BookRepository.java
@@ -16,4 +16,10 @@ public interface BookRepository extends MongoRepository<Book, String> {
      * Tries to find all the books of the given author
      */
     List<Book> findAllByAuthorIdsContaining(String authorId);
+
+    /**
+     * Tries to find all the books with assigned tag.
+     * Every book has a list of IDs dedicated to Tags.
+     */
+    List<Book> findAllByTagIdsContaining(String tagId);
 }
--- src/main/java/com/example/springmongographql/repository/PublisherRepository.java
+++ src/main/java/com/example/springmongographql/repository/PublisherRepository.java
@@ -3,5 +3,13 @@ package com.example.springmongographql.repository;
 import com.example.springmongographql.model.Publisher;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface PublisherRepository extends MongoRepository<Publisher, String> {
+
+    /**
+     * Gets the list of publishers with given tag assigned.
+     * Every publisher has a list of IDs dedicated to Tags.
+     */
+    List<Publisher> findAllByTagIdsContaining(String tagId);
 }
