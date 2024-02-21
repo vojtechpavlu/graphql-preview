7640899bce3f9ed92585a36ba28e422e143e5d2b
vojtechpavlu <vojtech.pavlu.dev@gmail.com>
2024-02-21 18:01:32 +0100

    Update tag service

--- src/main/java/com/example/springmongographql/service/TagService.java
+++ src/main/java/com/example/springmongographql/service/TagService.java
@@ -1,6 +1,12 @@
 package com.example.springmongographql.service;
 
+import com.example.springmongographql.model.Author;
+import com.example.springmongographql.model.Book;
+import com.example.springmongographql.model.Publisher;
 import com.example.springmongographql.model.Tag;
+import com.example.springmongographql.repository.AuthorRepository;
+import com.example.springmongographql.repository.BookRepository;
+import com.example.springmongographql.repository.PublisherRepository;
 import com.example.springmongographql.repository.TagRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,6 +19,9 @@ import java.util.Optional;
 public class TagService {
 
     private final TagRepository tagRepository;
+    private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
+    private final AuthorRepository authorRepository;
 
     public List<Tag> getAllTags() {
         return tagRepository.findAll();
@@ -24,5 +33,17 @@ public class TagService {
 
     public List<Tag> getMultipleTags(List<String> tagIds) {
         return tagRepository.findAllByIdIn(tagIds);
+    }
+
+    public List<Book> getAllBooksWithTag(String tagId) {
+        return bookRepository.findAllByTagIdsContaining(tagId);
+    }
+
+    public List<Publisher> getAllPublishersWithTag(String tagId) {
+        return publisherRepository.findAllByTagIdsContaining(tagId);
+    }
+
+    public List<Author> getAllAuthorsWithTag(String tagId) {
+        return authorRepository.findAllByTagIdsContaining(tagId);
     }
 }
